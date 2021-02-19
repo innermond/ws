d11a6bb1a43d811d4ead6df96d79a637aa4dd1f4
gabriel <innermond@yahoo.com>
2021-02-19 09:18:58 +0200

    record

--- pret.js
+++ pret.js
@@ -1,27 +1,94 @@
-var words_peaks = 45000;
-var dtp_hours = 7 * 24;
-var dtp_price = 0.65;
-function dtp(words_num) {
-    var ratio = words_peaks / words_num;
-    var atenuation = Math.min(Math.max(ratio, 0.9), 1);
-    var price = (words_num / 300) * dtp_price * atenuation;
-    var d = Math.ceil((dtp_hours / ratio * atenuation));
-    var hours = d % 24;
-    var days = (d - hours) / 24;
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+var __spreadArrays = (this && this.__spreadArrays) || function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+};
+function dh(d) {
+    var hours = d % 8;
+    var days = (d - hours) / 8;
     days += Math.floor(hours / 8);
     hours = hours % 8;
-    var time = { days: days, hours: hours };
+    return { days: days, hours: hours };
+}
+function undh(dt) {
+    var d = dt.days, h = dt.hours;
+    return d * 8 + h;
+}
+// scale val between intervals
+function scale(v, in_min, in_max, out_min, out_max) {
+    // assume in_min < in_max
+    var out = (v - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    return out;
+}
+function quota(num, peak, span, hours, price, tr) {
+    var ratio = peak / num;
+    var atenuation = scale.apply(void 0, __spreadArrays([ratio], tr));
+    price = (num / span) * price * atenuation;
+    var d = Math.ceil((hours / ratio * atenuation));
+    var time = dh(d);
     return { time: time, price: price };
 }
-function quotation(words_num, images_num, tables_num, lists_num) {
+function txt(num) {
+    return quota(num, 45000, 300, 7 * 8, 0.65, [0, 1, 0.75, 1]);
 }
-function dh(hours) {
-    var hours = d % 24;
-    var days = (d - hours) / 24;
-    days += Math.floor(hours / 8);
-    hours = hours % 8;
+function img(num) {
+    return quota(num, 10, 5, 2, 5, [0, 1, 0.8, 1]);
 }
-function testprice(vv) {
-    vv.forEach(function (v) { return console.log('words: ', v, 'got: ', dtp(v), ' is: ', v == dtp(v).price); });
+function lst(num) {
+    return quota(num, 10, 5, 2, 5, [0, 1, 0.9, 1]);
 }
-testprice([45000, 70000, 80000, 90000, 120000]);
+function tbl(num) {
+    return quota(num, 4, 2, 2, 4, [0, 1, 1, 1]);
+}
+function price(txt_num, img_num, lst_num, tbl_num) {
+    var qq = {
+        text: {},
+        pictures: {},
+        lists: {},
+        tables: {}
+    };
+    if (txt_num)
+        qq.text = txt(txt_num);
+    if (img_num)
+        qq.pictures = img(img_num);
+    if (lst_num)
+        qq.lists = lst(lst_num);
+    if (tbl_num)
+        qq.tables = tbl(tbl_num);
+    var price = 0.0;
+    var time = 0.0;
+    for (var k in qq) {
+        var q = qq[k];
+        var tm = undh(q.time);
+        price += q.price;
+        time += tm;
+    }
+    ;
+    return __assign(__assign({}, qq), { price: price, time: dh(time) });
+}
+[[30000, 20, 10, 3], [45000, 5, 5, 20]].forEach(function (pp) { return console.log(price.apply(void 0, pp)); });
+function testprice(vv, fnfn) {
+    vv.forEach(function (v) {
+        fnfn.forEach(function (fn) {
+            var q = fn(v);
+            console.log('words: ', v, 'quotation: ', q);
+        });
+    });
+}
+//testprice([1, 5000, 30000, 45000, 70000,  80000, 90000, 120000], [txt]);
+//testprice([1, 5, 10, 15, 30], [img]);
+//testprice([1, 5, 10, 15, 30], [lst]);
+//testprice([1, 5, 10, 15, 30], [tbl]);
--- pret.ts
+++ pret.ts
@@ -1,45 +1,102 @@
-const words_peaks: number = 45000;
-const dtp_hours: number = 7*24;
-const dtp_price: number = 0.65;
+interface DtpTime {
+	days: number,
+	hours: number,
+}
+
+function dh(d: number): DtpTime {
+	let hours: number = d%8;
+	let days: number = (d - hours)/8;
+	days += Math.floor(hours/8);
+	hours = hours%8;
+
+	return {days, hours};
+}
+
+function undh(dt: DtpTime): number {
+	let {days: d, hours: h} = dt;
+	return d*8 + h;
+}
 
 interface Quotation {
 	time: DtpTime,
 	price: number,
 }
 
-interface DtpTime {
-	days: number,
-	hours: number,
+// scale val between intervals
+function scale(v: number, in_min: number, in_max: number, out_min: number, out_max: number): number {
+	// assume in_min < in_max
+  let out = (v - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+	return out;
 }
 
-function dtp(words_num: number): Quotation {
-	const ratio: number = words_peaks/words_num;
-	const atenuation = Math.min(Math.max(ratio, 0.9), 1);
-	const price: number = (words_num/300)*dtp_price*atenuation;
-	const d: number = Math.ceil((dtp_hours/ratio*atenuation));
+function quota(num: number, peak: number, span: number, hours: number, price: number, tr:[number, number, number, number]): Quotation {
+	const ratio: number = peak/num;
+	const atenuation = scale(ratio, ...tr);
+	price = (num/span)*price*atenuation;
+	const d: number = Math.ceil((hours/ratio*atenuation));
 	
-	let hours: number = d%24;
-	let days: number = (d - hours)/24;
-	days += Math.floor(hours/8);
-	hours = hours%8;
-	
-	const time: DtpTime = {days, hours}
+	const time: DtpTime = dh(d);
 	return {time, price};
 }
 
-function quotation(words_num, images_num: number, tables_num: number, lists_num: number): Quotation {
-
+function txt(num: number): Quotation {
+	return quota(num, 45000, 300, 7*8, 0.65, [0, 1, 0.75, 1]);
 }
 
-function dh(hours: number): DtpTime {
-	let hours: number = d%24;
-	let days: number = (d - hours)/24;
-	days += Math.floor(hours/8);
-	hours = hours%8;
+function img(num: number): Quotation {
+	return quota(num, 10, 5, 2, 5, [0, 1, 0.8, 1]);
 }
 
-function testprice(vv:number[]) {
-	vv.forEach(v => console.log('words: ', v, 'got: ', dtp(v), ' is: ', v ==  dtp(v).price));
+function lst(num: number): Quotation {
+	return quota(num, 10, 5, 2, 5, [0, 1, 0.9, 1]);
 }
 
-testprice([45000, 70000,  80000, 90000, 120000]);
+function tbl(num: number): Quotation {
+	return quota(num, 4, 2, 2, 4, [0, 1, 1, 1]);
+}
+
+
+type Section = 'text'|'pictures'|'lists'|'tables';
+type QuotationSummary = Record<Section, Quotation> & {price: number, time: DtpTime};
+
+function price(txt_num?: number, img_num?: number, lst_num?: number, tbl_num?: number): QuotationSummary {
+	const qq: Record<Section, Quotation> = {
+		text: {} as Quotation,
+		pictures: {} as Quotation,
+		lists: {} as Quotation,
+		tables: {} as Quotation,
+	};
+	if (txt_num) qq.text = txt(txt_num);
+	if (img_num) qq.pictures = img(img_num);
+	if (lst_num) qq.lists = lst(lst_num);
+	if (tbl_num) qq.tables = tbl(tbl_num);
+
+	let price = 0.0;
+	let time = 0.0;
+	for (const k in qq) {
+		const q: Quotation = qq[k as Section];
+		const tm = undh(q.time);
+		price += q.price;
+		time += tm;
+	};
+	return {...qq, price, time: dh(time)};
+}
+
+[[30000, 20, 10, 3], [45000, 5, 5, 20]].forEach((pp) => console.log(price(...pp)));
+
+interface QuotationFunc {
+	(num: number): Quotation;
+}
+function testprice(vv:number[], fnfn: QuotationFunc[]): void {
+	vv.forEach(v => {
+		fnfn.forEach(fn => {
+			const q: Quotation = fn(v);
+			console.log('words: ', v, 'quotation: ', q);
+		})
+	})
+}
+
+//testprice([1, 5000, 30000, 45000, 70000,  80000, 90000, 120000], [txt]);
+//testprice([1, 5, 10, 15, 30], [img]);
+//testprice([1, 5, 10, 15, 30], [lst]);
+//testprice([1, 5, 10, 15, 30], [tbl]);
